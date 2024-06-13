5fba57f707f25b35494f0c2d1adf337a53806da5
hieu308 <musicgoddn2023@gmail.com>
2024-06-13 08:11:34 +0700

    up

--- ss7_bien_kieu_du_lieu/bai_2/js.js
+++ ss7_bien_kieu_du_lieu/bai_2/js.js
@@ -1,27 +1,25 @@
-function doSth() {
+function changeMoney() {
     let amount = document.getElementById("number").value;
-   let from = document.getElementById("choice").value;
-   let to = document.getElementById("pick").value;
-   let result;
-  if(from === "vn" && to === "us" && amount !== "" ){
-      result = amount/24000
-      document.getElementById("ab").innerHTML=`${result.toFixed(2)} USD`;
-  }else if(from === "us" && to === "vn" && amount !== "" ){
-      result = amount * 24000
-      document.getElementById("ab").innerHTML=`${result} VND`;
-  }
-  else if(from === "vn" && to === "vn" && amount !== "" ){
-      result = amount
-      document.getElementById("ab").innerHTML=`${result} VND`;
+    let from = document.getElementById("choice").value;
+    let to = document.getElementById("pick").value;
+    let result;
+    if (from === "vn" && to === "us" && amount !== "") {
+        result = amount / 24000
+        document.getElementById("ab").innerHTML = `${result.toFixed(2)} USD`;
+    } else if (from === "us" && to === "vn" && amount !== "") {
+        result = amount * 24000
+        document.getElementById("ab").innerHTML = `${result} VND`;
+    } else if (from === "vn" && to === "vn" && amount !== "") {
+        result = amount
+        document.getElementById("ab").innerHTML = `${result} VND`;
 
-  }
-  else if(from === "us" && to === "us" && amount !== ""){
-      result = amount
-      document.getElementById("ab").innerHTML=`${result} USD`;
+    } else if (from === "us" && to === "us" && amount !== "") {
+        result = amount
+        document.getElementById("ab").innerHTML = `${result} USD`;
 
-  } else if(amount === ""){
-      document.getElementById("ab").innerHTML=`Vui lòng nhập số tiền cần chuyển đổi`;
-  }
+    } else if (amount === "") {
+        document.getElementById("ab").innerHTML = `Vui lòng nhập số tiền cần chuyển đổi`;
+    }
 
 
 }
